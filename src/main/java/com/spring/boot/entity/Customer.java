734010a57a8f22e9a5da45486661d6d9e125e651
limityi <893911081@qq.com>
2018-06-25 16:58:38 +0800

    spring-data-jap

---
+++ src/main/java/com/spring/boot/entity/Customer.java
@@ -0,0 +1,35 @@
+package com.spring.boot.entity;
+
+import javax.persistence.*;
+
+/**
+ * 客户实体
+ * Created by XiongYi on 2018/06/25.
+ */
+@Table
+@Access(value = AccessType.FIELD)
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(length = 128)
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
